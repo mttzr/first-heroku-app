e80172e0e55b659d8e66d646ad2feec269a4291c
Matthew Zehnder <mz@uber.com>
2015-12-23 13:25:29 -0800

    Adding DB

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var cool = require('cool-ascii-faces');
 var app = express();
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,16 +31,17 @@ app.get('/cool', function(request, response){
 	response.send(cool());
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
 app.get('/', function(request, response) {
   var result = ''
   var times = process.env.TIMES || 5
   for (i=0; i < times; i++)
     result += cool();
   response.send(result);
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 
